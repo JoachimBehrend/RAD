491cdf7c992bccc3b7ce762fff5197b9d48035d9
jlb <jlb@zangenberg.biz>
2020-05-16 15:58:39 +0200

    file added

---
+++ implementationproject.cs
@@ -0,0 +1,44 @@
+# Implementation of hash tables 
+
+namespace Hashtables {
+    class LinkedHashEntry {
+        private int key;
+        private int value;
+        private LinkedHashEntry next; 
+        
+        LinkedHashEntry(int key, int value) {
+            this.key = key;
+            this.value = value; 
+            this.next = null; 
+        }
+
+        public int getValue() {
+            return this.value;
+        }
+
+        public void setValue(int newValue) {
+            this.value = newValue; 
+        }
+
+        public key getKey() {
+            return this.key;
+        }
+
+        public LinkedHashEntry getNext() {
+            return next; 
+        }
+
+        public void setNext(LinkedHashEntry next) {
+            this.next = next
+        }
+    }
+    
+
+
+}
+
+
+
+
+
+
